47de7a9f60abb6ca621de35e2093dcde605d5f39
Davids89 <daviluqui@gmail.com>
2016-12-28 11:06:04 -0800

    DBFlow in application class

--- app/src/main/java/com/example/david/myrecipes/FacebookRecipesApp.java
+++ app/src/main/java/com/example/david/myrecipes/FacebookRecipesApp.java
@@ -3,6 +3,7 @@ package com.example.david.myrecipes;
 import android.app.Application;
 
 import com.facebook.FacebookSdk;
+import com.raizlabs.android.dbflow.config.FlowManager;
 
 /**
  * Created by david on 28/12/16.
@@ -14,6 +15,21 @@ public class FacebookRecipesApp extends Application {
     public void onCreate() {
         super.onCreate();
         initFacebook();
+        initDB();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        DBTearDown();
+    }
+
+    private void DBTearDown() {
+        FlowManager.destroy();
+    }
+
+    private void initDB() {
+        FlowManager.init(this);
     }
 
     private void initFacebook() {
