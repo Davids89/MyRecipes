cb1561470e2a67e7cb023612375b411e7dd26c4f
Davids89 <daviluqui@gmail.com>
2016-12-29 17:59:36 -0800

    Login implemented

--- app/src/main/java/com/example/david/myrecipes/main/ui/RecipeMainActivity.java
+++ app/src/main/java/com/example/david/myrecipes/main/ui/RecipeMainActivity.java
@@ -1,13 +1,15 @@
-package com.example.david.myrecipes;
+package com.example.david.myrecipes.main.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.david.myrecipes.R;
+
+public class RecipeMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
